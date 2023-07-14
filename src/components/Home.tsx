393e8674aa25b298495f92b2a87e75a7e936f65f
Saket Sinha <saket.sinha89@gmail.com>
2023-07-14 23:27:22 +0200

    New commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,30 +17,16 @@ const RepoReadmeLink: React.VFC = () => (
 export const Home: React.VFC = () => (
   <div>
     <P>
-      This is an example single page app built with React and React&nbsp;Router
-      using <code>BrowserRouter</code>. Navigate with the links below and
-      refresh the page or copy/paste the url to test out the redirect
-      functionality deployed to overcome GitHub&nbsp;Pages incompatibility with
-      single page apps (like this one).
-    </P>
-    <P>
-      Please see the <RepoReadmeLink /> for instructions on how to use this
-      boilerplate to deploy your own single page app using GitHub Pages.
-    </P>
-    <P>
-      <LinkContainer>
-        <InteractiveLink to="/example">Example page</InteractiveLink>
-      </LinkContainer>
-      <LinkContainer>
-        <InteractiveLink to="/example/two-deep?field1=foo&field2=bar#boom!">
-          Example two deep with query and hash
-        </InteractiveLink>
-      </LinkContainer>
-    </P>
-    <P>
-      <InteractiveLink to="/sitemap-link-generator">
-        Sitemap Link Generator
-      </InteractiveLink>
+      CAN Controller Bringup with Litex SOC The task’s aim is to bringup a Litex
+      based SOC with a CAN controller. The effort involved in the scope of this
+      task is to integrate an existing open source CAN controller IP in FPGA and
+      make it functional with required device drivers. Components involved - 
+      CAN core IP based on an MIT-licensed variant of the CTU CAN FD Controller
+      developed by the Czech Technical University in Prague.  RISC-V softcore
+      based on LiteX, a Python-based System on a Chip (SoC) designer for open
+      source supported Field Programmable Gate Array (FPGA).  Device Driver
+      based on SocketCAN, a standard common interface for CAN devices in the
+      Linux kernel.
     </P>
   </div>
 );
