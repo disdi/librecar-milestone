e14d8d47eae8a857d9849a7084715b3c40ac2de5
Saket Sinha <saket.sinha89@gmail.com>
2023-07-14 22:57:52 +0200

    Update boilerplate for use with my domain

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/librecar-milestone" >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
